119b0b526b61de99d1f08f30ac953af4e5054653
XorMasters <aknight@vcccd.edu>
2018-09-17 13:11:48 -0700

    Turning in

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,14 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 33323
+* Assignment: EX01-LinearSearch
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author aknight@vcccd.edu
+*/
+
 #include <iostream>
 #include "linearsearch.h"
 
